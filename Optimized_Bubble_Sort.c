40255814023ff00ad4f82608a1f24d0e165aa4ba
Ishan Hansaka <ishanhansakasilva@gmail.com>
2024-08-13 17:08:02 +0530

    chore: add comments to better understanding

--- Optimized_Bubble_Sort.c
+++ Optimized_Bubble_Sort.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-#include<stdbool.h>
+#include <stdbool.h>
 
 void bubbleSort(int arr[], int n);
-void display (int arr[], int n);
+void display(int arr[], int n);
 
 void bubbleSort(int arr[], int n) {
     int temp;
-    bool isSwaped;
+    bool isSwaped; //Flag to check if any elements were swapped during the pass
+
     for (int i = 0; i < n-1; i++) {
-        isSwaped = false;
+        isSwaped = false; //Reset the swap flag at the beginning of each pass
+
         for (int j = 0; j < n-i-1; j++) {
             if (arr[j] > arr[j+1]) {
                 temp = arr[j];
@@ -17,11 +19,13 @@ void bubbleSort(int arr[], int n) {
                 isSwaped = true;
             }
         }
+
+        //If no elements were swapped in the inner loop, the array is already sorted
         if (isSwaped == false) break;
     }
 }
 
-void display (int arr[], int n) {
+void display(int arr[], int n) {
     for (int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
@@ -29,7 +33,7 @@ void display (int arr[], int n) {
 
 int main() {
     int arr[] = {4, 3, 2, 10, 12, 1, 5, 6};
-    int length = sizeof(arr)/sizeof(arr[0]);
+    int length = sizeof(arr) / sizeof(arr[0]);
     printf("Before Sort => ");
     display(arr, length);
     bubbleSort(arr, length);
